4204562c9ab9dbd35a4bc8febce63074221ebcac
Spaguetto <48161646+Spaguetto@users.noreply.github.com>
2023-09-18 14:14:02 -0500

    first commit

---
+++ src/Card.jsx
@@ -0,0 +1,23 @@
+export default () => (
+  <div className="birthdayCard">
+    <div className="cardFront">
+      <h3 className="happy">Feliz cumpleaños Jhon!!</h3>
+      <div className="balloons">
+        <div className="balloonOne" />
+        <div className="balloonTwo" />
+        <div className="balloonThree" />
+        <div className="balloonFour" />
+      </div>
+    </div>
+    <div className="cardInside">
+      <h3 className="back">Al mejor Sr Dev que conozco:</h3>
+      <p>Querido Jhon,</p>
+      <p>
+        Espero que donde quiera que estes estes pasando un super feliz
+        cumpleaños, gracias por guiarme y enseñarme mucho de lo que se, pasala
+        super!
+      </p>
+      <p className="name">Pawan</p>
+    </div>
+  </div>
+);
